5d5f40a01df5d6713569e0a39f24e9833867f1b5
Александр Захаров <zaher0861@yandex.ru>
2021-11-29 23:26:08 +0300

    Добавьте файлы проекта.

---
+++ Lesson_6.2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = Console.ReadLine();
+            string[] s1Array = s1.Split();
+            string s2 = "";
+            int n;
+            foreach (string s in s1Array)
+            {
+                s2 += s;
+            }
+            n = s2.Length;
+            for (int i = 0; i < n; i++)
+            {
+                if (s2[i] == s2[n-1-i])
+                {
+                    if (i == n-1)
+                    {
+                        Console.WriteLine("Ваше предложение - палиндром! Поздравяю!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ваше предложение не является палиндромом!");
+                    break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson_6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = Console.ReadLine();
+            string[] s1Array = s1.Split();
+            int length = 0;
+            string longest = "";
+            foreach (string s in s1Array)
+            {
+                if (s.Length>length)
+                {
+                    length = s.Length;
+                    longest = s;
+                }
+            }
+            Console.WriteLine("Самое длинное слово - {0}, его длина - {1}", longest, length);
+            Console.ReadKey();
+        }
+    }
+}
